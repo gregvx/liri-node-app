3d312be5334dd6aa35bf3369284a2676169f2458
gregvx <32096447+gregvx@users.noreply.github.com>
2018-11-02 01:21:28 -0600

    added logic to main app.

--- liri.js
+++ liri.js
@@ -4,11 +4,42 @@ require("dotenv").config();
 var keys = require('./keys.js');
  
 var spotify = new Spotify(keys.spotify);
+
+
+//parse out command
+var command = process.argv[2];
+var remainder = "";
+for (var i=3; i < process.argv.length; i++) {
+    remainder = remainder + process.argv[i] + " ";
+}
+remainder = remainder.trim();
+remainder = remainder.split(' ').join('+');
+
+console.log("The command you entered was " + command + " as related to " + remainder);
  
-spotify.search({ type: 'track', query: 'Porcelina' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+if (command === "concert-this") {
+    var baseurl = "https://rest.bandsintown.com/artists/" + remainder + "/events?app_id=codingbootcamp";
+    console.log(baseurl);
+    request(baseurl, function(err, res, body) {
+        console.log(body);
+    });
+}
+
+if (command === "spotify-this-song") {
+    
+}
+
+if (command === "movie-this") {
+    
+}
+
+if (command === "do-what-it-says") {
+    
+}
+// spotify.search({ type: 'track', query: 'Porcelina' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
  
-console.log(data.tracks); 
-});
+// console.log(data.tracks); 
+// });
